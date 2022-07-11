a184affc2746bc501018edebb20b671bd6551bae
yuk1ty <yuk1ty@users.noreply.github.com>
2022-07-11 09:23:11 +0900

    add for

---
+++ zig/basics/for.zig
@@ -0,0 +1,18 @@
+const expect = @import("std").testing.expect;
+
+test "for" {
+    const string = [_]u8{ 'a', 'b', 'c' };
+
+    for (string) |character, index| {
+        _ = character;
+        _ = index;
+    }
+
+    for (string) |character| {
+        _ = character;
+    }
+
+    for (string) |_, index| {
+        _ = index;
+    }
+}
