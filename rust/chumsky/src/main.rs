ca04d4fc03a8ab46f192aef3cdae63d2c202a298
yuk1ty <yuk1ty@users.noreply.github.com>
2022-07-18 00:50:40 +0900

    binary operators

--- rust/chumsky/src/main.rs
+++ rust/chumsky/src/main.rs
@@ -29,16 +29,42 @@ enum Expr {
 }
 
 fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
-    let int = text::int(10)
-        .map(|s: String| Expr::Num(s.parse().unwrap()))
-        .padded();
-    let atom = int;
-    let op = |c| just(c).padded();
-    let unary = op('-')
-        .repeated()
-        .then(atom)
-        .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
-    unary.then_ignore(end())
+    recursive(|expr| {
+        let int = text::int(10)
+            .map(|s: String| Expr::Num(s.parse().unwrap()))
+            .padded();
+        let atom = int.or(expr.delimited_by(just('('), just(')')));
+        let op = |c| just(c).padded();
+        let unary = op('-')
+            .repeated()
+            .then(atom)
+            .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
+
+        let product = unary
+            .clone()
+            .then(
+                op('*')
+                    .to(Expr::Mul as fn(_, _) -> _)
+                    .or(op('/').to(Expr::Div as fn(_, _) -> _))
+                    .then(unary)
+                    .repeated(),
+            )
+            .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+
+        let sum = product
+            .clone()
+            .then(
+                op('+')
+                    .to(Expr::Add as fn(_, _) -> _)
+                    .or(op('-').to(Expr::Sub as fn(_, _) -> _))
+                    .then(product)
+                    .repeated(),
+            )
+            .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+
+        sum
+    })
+    .then_ignore(end())
 }
 
 fn eval(expr: &Expr) -> Result<f64, String> {
