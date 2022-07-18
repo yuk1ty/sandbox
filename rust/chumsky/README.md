17ad92e26347a60f42f528980fb076296c72d626
yuk1ty <yuk1ty@users.noreply.github.com>
2022-07-18 16:37:48 +0900

    complete tutorial

---
+++ rust/chumsky/README.md
@@ -0,0 +1,2 @@
+# chumsky-tutorial
+https://github.com/zesterer/chumsky/blob/master/tutorial.md
--- rust/chumsky/src/main.rs
+++ rust/chumsky/src/main.rs
@@ -34,7 +34,18 @@ fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
         let int = text::int(10)
             .map(|s: String| Expr::Num(s.parse().unwrap()))
             .padded();
-        let atom = int.or(expr.delimited_by(just('('), just(')')));
+        let call = ident
+            .then(
+                expr.clone()
+                    .separated_by(just(','))
+                    .allow_trailing()
+                    .delimited_by(just('('), just(')')),
+            )
+            .map(|(f, args)| Expr::Call(f, args));
+        let atom = int
+            .or(expr.delimited_by(just('('), just(')')))
+            .or(call)
+            .or(ident.map(Expr::Var));
         let op = |c| just(c).padded();
         let unary = op('-')
             .repeated()
@@ -73,26 +84,44 @@ fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
             .then_ignore(just('='))
             .then(expr.clone())
             .then_ignore(just(';'))
-            .then(decl)
+            .then(decl.clone())
             .map(|((name, rhs), then)| Expr::Let {
                 name,
                 rhs: Box::new(rhs),
                 then: Box::new(then),
             });
 
-        r#let.or(expr).padded()
+        let r#fn = text::keyword("fn")
+            .ignore_then(ident)
+            .then(ident.repeated())
+            .then_ignore(just('='))
+            .then(expr.clone())
+            .then_ignore(just(';'))
+            .then(decl)
+            .map(|(((name, args), body), then)| Expr::Fn {
+                name,
+                args,
+                body: Box::new(body),
+                then: Box::new(then),
+            });
+
+        r#let.or(r#fn).or(expr).padded()
     });
     decl.then_ignore(end())
 }
 
-fn eval<'a>(expr: &'a Expr, vars: &mut Vec<(&'a String, f64)>) -> Result<f64, String> {
+fn eval<'a>(
+    expr: &'a Expr,
+    vars: &mut Vec<(&'a String, f64)>,
+    funcs: &mut Vec<(&'a String, &'a [String], &'a Expr)>,
+) -> Result<f64, String> {
     match expr {
         Expr::Num(x) => Ok(*x),
-        Expr::Neg(a) => Ok(-eval(a, vars)?),
-        Expr::Add(a, b) => Ok(eval(a, vars)? + eval(b, vars)?),
-        Expr::Sub(a, b) => Ok(eval(a, vars)? - eval(b, vars)?),
-        Expr::Mul(a, b) => Ok(eval(a, vars)? * eval(b, vars)?),
-        Expr::Div(a, b) => Ok(eval(a, vars)? / eval(b, vars)?),
+        Expr::Neg(a) => Ok(-eval(a, vars, funcs)?),
+        Expr::Add(a, b) => Ok(eval(a, vars, funcs)? + eval(b, vars, funcs)?),
+        Expr::Sub(a, b) => Ok(eval(a, vars, funcs)? - eval(b, vars, funcs)?),
+        Expr::Mul(a, b) => Ok(eval(a, vars, funcs)? * eval(b, vars, funcs)?),
+        Expr::Div(a, b) => Ok(eval(a, vars, funcs)? / eval(b, vars, funcs)?),
         Expr::Var(name) => {
             if let Some((_, val)) = vars.iter().rev().find(|(var, _)| *var == name) {
                 Ok(*val)
@@ -101,13 +130,50 @@ fn eval<'a>(expr: &'a Expr, vars: &mut Vec<(&'a String, f64)>) -> Result<f64, St
             }
         }
         Expr::Let { name, rhs, then } => {
-            let rhs = eval(rhs, vars)?;
+            let rhs = eval(rhs, vars, funcs)?;
             vars.push((name, rhs));
-            let output = eval(then, vars);
+            let output = eval(then, vars, funcs);
             vars.pop();
             output
         }
-        _ => todo!(),
+        Expr::Call(name, args) => {
+            if let Some((_, arg_names, body)) =
+                funcs.iter().rev().find(|(var, _, _)| *var == name).copied()
+            {
+                if arg_names.len() == args.len() {
+                    let mut args = args
+                        .iter()
+                        .map(|arg| eval(arg, vars, funcs))
+                        .zip(arg_names.iter())
+                        .map(|(val, name)| Ok((name, val?)))
+                        .collect::<Result<_, String>>()?;
+                    vars.append(&mut args);
+                    let output = eval(body, vars, funcs);
+                    vars.truncate(vars.len() - args.len());
+                    output
+                } else {
+                    Err(format!(
+                        "Wrong number of arguments for function `{}`: expected {}, found {}",
+                        name,
+                        arg_names.len(),
+                        args.len(),
+                    ))
+                }
+            } else {
+                Err(format!("Cannot find function `{}` in scope", name))
+            }
+        }
+        Expr::Fn {
+            name,
+            args,
+            body,
+            then,
+        } => {
+            funcs.push((name, args, body));
+            let output = eval(then, vars, funcs);
+            funcs.pop();
+            output
+        }
     }
 }
 
@@ -115,7 +181,7 @@ fn main() {
     let src = read_to_string(args().nth(1).unwrap()).unwrap();
 
     match parser().parse(src) {
-        Ok(ast) => match eval(&ast, &mut Vec::new()) {
+        Ok(ast) => match eval(&ast, &mut Vec::new(), &mut Vec::new()) {
             Ok(output) => println!("{}", output),
             Err(eval_err) => println!("Evaluation error: {}", eval_err),
         },
@@ -191,4 +257,15 @@ fn parse_binary() {
             Box::new(Expr::Num(2.0))
         ))
     );
+}
+
+#[test]
+fn parse_program() {
+    let text = r#"let five = 5;
+    let eight = 3 + five;
+    fn add x y = x + y;
+    add(five, eight)
+    "#;
+    let result = parser().parse(text);
+    assert!(matches!(result, Ok(_)));
 }
