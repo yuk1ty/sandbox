771407deb282b07dabbe6b8a93340fb0e383f615
yuk1ty <yuk1ty@users.noreply.github.com>
2022-09-02 12:52:23 +0900

    ready to run graphql server

--- ts/graphql-server/src/index.ts
+++ ts/graphql-server/src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello, TypeScript");
+import { createServer } from "@graphql-yoga/node";
+
+const port = Number(process.env.API_PORT) || 4000;
+const server = createServer({ port });
+server.start().then(() => {
+  console.log(`🚀 GraphSQL Server ready at http://localhost:${port}/graphql`);
+});
