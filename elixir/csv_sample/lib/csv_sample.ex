417a7b7216bbff77004ec3a3596f499a5dc26e1e
yuk1ty <yuk1ty@users.noreply.github.com>
2022-10-24 01:08:57 +0900

    add first elixir

---
+++ elixir/csv_sample/lib/csv_sample.ex
@@ -0,0 +1,18 @@
+defmodule CsvSample do
+  @moduledoc """
+  Documentation for `CsvSample`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CsvSample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/csv_sample/test/csv_sample_test.exs
@@ -0,0 +1,8 @@
+defmodule CsvSampleTest do
+  use ExUnit.Case
+  doctest CsvSample
+
+  test "greets the world" do
+    assert CsvSample.hello() == :world
+  end
+end
