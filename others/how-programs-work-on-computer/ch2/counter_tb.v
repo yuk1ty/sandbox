89af14be40be8dd5f051d71d2c54d1f32f3b37a6
yuk1ty <yuk1ty@users.noreply.github.com>
2022-11-20 22:41:11 +0900

    add state

--- others/how-programs-work-on-computer/ch2/counter_tb.v
+++ others/how-programs-work-on-computer/ch2/counter_tb.v
@@ -9,9 +9,9 @@ module counter_tb;
     $dumpvars;
     rst_n=0; load=0; inc=0; d=0; #100
     rst_n=1; inc=1; #200
-    inc=0; load=1; d=123; #100
+    inc=0; load=1; d=123; #100 // write
     inc=1; load=0; #200
-    inc=0; load=1; d=254; #100
+    inc=0; load=1; d=254; #100 // write
     inc=1; load=0; #300
     rst_n=0; #100
     $finish;
