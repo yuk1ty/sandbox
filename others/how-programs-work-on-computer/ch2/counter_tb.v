f0e5fd586e57cbf36d0effb09cb06eec952962d9
yuk1ty <yuk1ty@users.noreply.github.com>
2022-11-20 22:11:21 +0900

    add counter

---
+++ others/how-programs-work-on-computer/ch2/counter_tb.v
@@ -0,0 +1,19 @@
+module counter_tb;
+  reg clk, rst_n, load, inc;
+  reg [7:0] d;
+  wire [7:0] q;
+
+  counter #(8) counter0(clk, rst_n, load, inc, d, q);  
+
+  initial begin
+    $dumpvars;
+    rst_n=0; load=0; inc=0; d=0; #100
+    rst_n=1; inc=1; #200
+    inc=0; load=1; d=123; #100
+    inc=1; load=0; #200
+    inc=0; load=1; d=254; #100
+    inc=1; load=0; #300
+    rst_n=0; #100
+    $finish;
+  end
+endmodule
