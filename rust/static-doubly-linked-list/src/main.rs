e7a63518baaae0a08ee9987d51ba5f8db0717714
yuk1ty <yuk1ty@users.noreply.github.com>
2023-05-20 02:11:27 +0900

    add pop_back and clear

--- rust/static-doubly-linked-list/src/main.rs
+++ rust/static-doubly-linked-list/src/main.rs
@@ -30,7 +30,6 @@ impl<'id, T> DoublyLinkedList<'id, T> {
                 old_head.borrow_mut(token).prev = Some(itself);
                 cloned.borrow_mut(token).next = Some(old_head);
                 self.head = Some(cloned);
-                self.tail = self.tail.take();
                 self.len += 1;
             }
             None => {
@@ -57,6 +56,28 @@ impl<'id, T> DoublyLinkedList<'id, T> {
                 self.len = 1;
             }
         }
+    }
+
+    pub fn pop_back(&mut self, token: &mut GhostToken<'id>) -> Option<T> {
+        self.tail.take().map(|old_tail| {
+            match old_tail.borrow_mut(token).prev.take() {
+                Some(new_tail) => {
+                    new_tail.borrow_mut(token).next.take();
+                    self.tail = Some(new_tail);
+                }
+                None => {
+                    self.head = None;
+                }
+            }
+            let full = StaticRc::adjust::<2, 2>(old_tail);
+            let ghost_cell = StaticRc::into_inner(full);
+            let node = GhostCell::into_inner(ghost_cell);
+            node.data
+        })
+    }
+
+    pub fn clear(&mut self, token: &mut GhostToken<'id>) {
+        while self.pop_back(token).is_some() {}
     }
 }
 
@@ -90,17 +111,19 @@ mod tests {
     fn add_sequential_numbers_to_existing_list_and_get_four_elements() {
         GhostToken::new(|mut token| {
             let mut list = DoublyLinkedList::new();
-            println!("1");
             list.push_front(1, &mut token);
             list.push_front(2, &mut token);
             list.push_front(3, &mut token);
             list.push_front(4, &mut token);
-            println!("2");
+
             assert_eq!(list.len, 4);
-            println!("3");
-            println!("{:?}", list);
+            assert_eq!(list.pop_back(&mut token).unwrap(), 4);
+            assert_eq!(list.pop_back(&mut token).unwrap(), 3);
+            assert_eq!(list.pop_back(&mut token).unwrap(), 2);
+            assert_eq!(list.pop_back(&mut token).unwrap(), 1);
+
+            list.clear(&mut token);
         });
-        println!("4");
     }
 }
 
