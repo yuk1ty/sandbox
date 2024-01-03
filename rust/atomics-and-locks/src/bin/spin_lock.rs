5104df8877c867ff6f60e6978934c77fc355743a
yuk1ty <yuk1ty@users.noreply.github.com>
2024-01-03 21:46:00 +0900

    add spin lock

---
+++ rust/atomics-and-locks/src/bin/spin_lock.rs
@@ -0,0 +1,72 @@
+use std::{
+    cell::UnsafeCell,
+    ops::{Deref, DerefMut},
+    sync::atomic::AtomicBool,
+    thread,
+};
+
+pub struct SpinLock<T> {
+    locked: AtomicBool,
+    value: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for SpinLock<T> where T: Send {}
+
+impl<T> SpinLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn lock(&self) -> Guard<T> {
+        while self.locked.swap(true, std::sync::atomic::Ordering::Acquire) {
+            std::hint::spin_loop();
+        }
+        Guard { lock: self }
+    }
+}
+
+pub struct Guard<'a, T> {
+    lock: &'a SpinLock<T>,
+}
+
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.lock.value.get() }
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.lock.value.get() }
+    }
+}
+
+impl<T> Drop for Guard<'_, T> {
+    fn drop(&mut self) {
+        self.lock
+            .locked
+            .store(false, std::sync::atomic::Ordering::Release);
+    }
+}
+
+unsafe impl<T> Send for Guard<'_, T> where T: Send {}
+unsafe impl<T> Sync for Guard<'_, T> where T: Sync {}
+
+fn main() {
+    let x = SpinLock::new(Vec::new());
+    thread::scope(|s| {
+        s.spawn(|| x.lock().push(1));
+        s.spawn(|| {
+            let mut g = x.lock();
+            g.push(2);
+            g.push(2);
+        });
+    });
+    let g = x.lock();
+    assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
+}
