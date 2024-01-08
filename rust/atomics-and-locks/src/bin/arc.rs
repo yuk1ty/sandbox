f22a9cf4cc932696e4c9f0a50332087b8c72b77e
yuk1ty <yuk1ty@users.noreply.github.com>
2024-01-08 21:22:19 +0900

    applied loom for tests

--- rust/atomics-and-locks/src/bin/arc.rs
+++ rust/atomics-and-locks/src/bin/arc.rs
@@ -28,6 +28,9 @@ impl<T> Arc<T> {
         unsafe { self.ptr.as_ref() }
     }
 
+    // DerefMutのように、無条件に&mut
+    // Tを約束するわけには行かないが、一方で参照カウントが1だったときかつ、
+    // 他のArcオブジェクトが他のデータにアクセスしないと保証できれば、&mut Tを返すメソッドを定義することができる。
     pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
         if arc.data().ref_count.load(Ordering::Relaxed) == 1 {
             atomic::fence(Ordering::Acquire);
@@ -72,42 +75,97 @@ unsafe impl<T: Send + Sync> Sync for Arc<T> {}
 
 fn main() {}
 
-#[test]
-fn test() {
-    static NUM_DROPS: AtomicUsize = AtomicUsize::new(0);
+#[cfg(test)]
+mod tests {
+    use loom::lazy_static::Lazy;
+    use loom::sync::atomic::AtomicUsize;
+    use loom::sync::atomic::Ordering::{Acquire, Relaxed, Release, SeqCst};
+    use loom::thread;
+    use std::marker::PhantomData;
 
-    struct DetectDrop;
+    use crate::Arc;
 
-    impl Drop for DetectDrop {
-        fn drop(&mut self) {
-            NUM_DROPS.fetch_add(1, Ordering::Relaxed);
+    #[test]
+    fn test() {
+        // static NUM_DROPS: AtomicUsize = AtomicUsize::new(0);
+        static NUM_DROPS: Lazy<AtomicUsize> = Lazy {
+            init: || AtomicUsize::new(0),
+            _p: PhantomData,
+        };
+
+        struct DetectDrop;
+
+        impl Drop for DetectDrop {
+            fn drop(&mut self) {
+                NUM_DROPS.get().fetch_add(1, Relaxed);
+            }
         }
+
+        loom::model(|| {
+            // Create two Arcs sharing an object containing a string
+            // and a DetectDrop, to detect when it's dropped.
+            let x = Arc::new(("hello", DetectDrop));
+            let y = x.clone();
+
+            // Send x to another thread, and use it there.
+            let t = thread::spawn(move || {
+                assert_eq!(x.0, "hello");
+            });
+
+            // In parallel, y should still be usable here.
+            assert_eq!(y.0, "hello");
+
+            // Wait for the thread to finish.
+            t.join().unwrap();
+
+            // One Arc, x, should be dropped by now.
+            // We still have y, so the object shouldn't have been dropped yet.
+            assert_eq!(NUM_DROPS.get().load(Relaxed), 0);
+
+            // Drop the remaining `Arc`.
+            drop(y);
+
+            // Now that `y` is dropped too,
+            // the object should've been dropped.
+            assert_eq!(NUM_DROPS.get().load(Relaxed), 1);
+        });
     }
 
-    // Create two Arcs sharing an object containing a string
-    // and a DetectDrop, to detect when it's dropped.
-    let x = Arc::new(("hello", DetectDrop));
-    let y = x.clone();
+    #[test]
+    #[should_panic]
+    fn test_concurrent_logic_1() {
+        loom::model(|| {
+            let v1 = Arc::new(AtomicUsize::new(0));
+            let v2 = v1.clone();
+            thread::spawn(move || {
+                v1.store(1, SeqCst);
+            });
 
-    // Send x to another thread, and use it there.
-    let t = std::thread::spawn(move || {
-        assert_eq!(x.0, "hello");
-    });
+            assert_eq!(0, v2.load(SeqCst));
+        });
+    }
 
-    // In parallel, y should still be usable here.
-    assert_eq!(y.0, "hello");
+    #[test]
+    #[should_panic]
+    fn test_concurrent_logic_2() {
+        loom::model(|| {
+            let num = Arc::new(AtomicUsize::new(0));
 
-    // Wait for the thread to finish.
-    t.join().unwrap();
+            let ths: Vec<_> = (0..2)
+                .map(|_| {
+                    let num = num.clone();
+                    thread::spawn(move || {
+                        let curr = num.load(Acquire);
+                        num.store(curr + 1, Release);
+                    })
+                })
+                .collect();
 
-    // One Arc, x, should be dropped by now.
-    // We still have y, so the object shouldn't have been dropped yet.
-    assert_eq!(NUM_DROPS.load(Ordering::Relaxed), 0);
+            for th in ths {
+                th.join().unwrap();
+            }
 
-    // Drop the remaining `Arc`.
-    drop(y);
-
-    // Now that `y` is dropped too,
-    // the object should've been dropped.
-    assert_eq!(NUM_DROPS.load(Ordering::Relaxed), 1);
+            assert_eq!(2, num.load(Relaxed));
+        });
+    }
 }
