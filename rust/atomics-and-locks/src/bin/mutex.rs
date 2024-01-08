fd1204242d01b72281b173c5459115b05980a605
yuk1ty <yuk1ty@users.noreply.github.com>
2024-01-09 07:51:43 +0900

    mutex

---
+++ rust/atomics-and-locks/src/bin/mutex.rs
@@ -0,0 +1,61 @@
+use atomic_wait::{wait, wake_one};
+use std::{
+    cell::UnsafeCell,
+    ops::{Deref, DerefMut},
+    sync::atomic::{AtomicU32, Ordering},
+};
+
+pub struct Mutex<T> {
+    // 0: unlocked
+    // 1: locked
+    state: AtomicU32,
+    value: UnsafeCell<T>,
+}
+
+impl<T> Mutex<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            state: AtomicU32::new(0), // unlocked
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn lock(&self) -> MutexGuard<T> {
+        while self.state.swap(1, Ordering::Acquire) == 1 {
+            wait(&self.state, 1);
+        }
+        MutexGuard { mutex: self }
+    }
+}
+
+unsafe impl<T> Sync for Mutex<T> where T: Send {}
+
+pub struct MutexGuard<'a, T> {
+    mutex: &'a Mutex<T>,
+}
+
+unsafe impl<T> Sync for MutexGuard<'_, T> where T: Sync {}
+
+impl<T> Deref for MutexGuard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { &*self.mutex.value.get() }
+    }
+}
+
+impl<T> DerefMut for MutexGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.mutex.value.get() }
+    }
+}
+
+impl<T> Drop for MutexGuard<'_, T> {
+    fn drop(&mut self) {
+        self.mutex.state.store(0, Ordering::Release);
+        // 待機中のスレッドがあればそのひとつを起こす。
+        wake_one(&self.mutex.state);
+    }
+}
+
+fn main() {}
