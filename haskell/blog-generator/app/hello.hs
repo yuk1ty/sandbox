d8f9b3dabcaec56d0cb169f50cf264561dcf24a6
yuk1ty <yuk1ty@users.noreply.github.com>
2024-10-20 20:49:49 +0900

    Split files

--- haskell/blog-generator/app/hello.hs
+++ haskell/blog-generator/app/hello.hs
@@ -1,3 +1,5 @@
+import Html
+
 main :: IO ()
 main = putStrLn (render myhtml)
 
@@ -13,42 +15,3 @@ myhtml =
         )
     )
 
-newtype Html = Html String
-
-newtype Structure = Structure String
-
-type Title = String
-
-html_ :: Title -> Structure -> Html
-html_ title content =
-  Html
-    ( el
-        "html"
-        ( el "head" (el "title" title)
-            <> el "body" (getStructureString content)
-        )
-    )
-
-p_ :: String -> Structure
-p_ = Structure . el "p"
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1"
-
-el :: String -> String -> String
-el tag content =
-  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-getStructureString :: Structure -> String
-getStructureString struct =
-  case struct of
-    Structure str -> str
-
-append_ :: Structure -> Structure -> Structure
-append_ (Structure a) (Structure b) =
-  Structure (a <> b)
-
-render :: Html -> String
-render html =
-  case html of
-    Html str -> str
