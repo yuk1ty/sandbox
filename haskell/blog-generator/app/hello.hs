db6cbcf541a126f7873e9b0cf7952e1504e07256
yuk1ty <yuk1ty@users.noreply.github.com>
2024-10-20 12:19:18 +0900

    Add structure and append!

--- haskell/blog-generator/app/hello.hs
+++ haskell/blog-generator/app/hello.hs
@@ -1,33 +1,39 @@
 main :: IO ()
-main = putStrLn myhtml
+main = putStrLn (render myhtml)
 
-myhtml :: String
-myhtml = makeHtml "Hello title" (h1_ "Hello, title" <> p_ "Let's learn about Haskell")
-
-makeHtml :: String -> String -> String
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+myhtml :: Html
+myhtml =
+  html_
+    "My title"
+    ( append_
+        (h1_ "Heading")
+        ( append_
+            (p_ "Paragraph #1")
+            (p_ "Paragraph #2")
+        )
+    )
 
 newtype Html = Html String
 
 newtype Structure = Structure String
 
-html_ :: String -> String
-html_ = el "html"
+type Title = String
 
-head_ :: String -> String
-head_ = el "head"
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html
+    ( el
+        "html"
+        ( el "head" (el "title" title)
+            <> el "body" (getStructureString content)
+        )
+    )
 
-title_ :: String -> String
-title_ = el "title"
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
-body_ :: String -> String
-body_ = el "body"
-
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
 el :: String -> String -> String
 el tag content =
@@ -37,3 +43,12 @@ getStructureString :: Structure -> String
 getStructureString struct =
   case struct of
     Structure str -> str
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) =
+  Structure (a <> b)
+
+render :: Html -> String
+render html =
+  case html of
+    Html str -> str
