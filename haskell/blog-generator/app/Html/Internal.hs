55599794e46439eea3c8d1e00ca028b1fd553324
yuk1ty <yuk1ty@users.noreply.github.com>
2024-10-26 12:19:07 +0900

    Module

---
+++ haskell/blog-generator/app/Html/Internal.hs
@@ -0,0 +1,56 @@
+module Html.Internal where
+
+newtype Html = Html String
+
+newtype Structure = Structure String
+
+type Title = String
+
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html
+    ( el
+        "html"
+        ( el "head" (el "title" (escape title))
+            <> el "body" (getStructureString content)
+        )
+    )
+
+p_ :: String -> Structure
+p_ = Structure . el "p" . escape
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1" . escape
+
+ul_ :: [Structure] -> Structure
+ul_ =
+  Structure . el "ul" . concatMap (el "li" . getStructureString)
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+getStructureString :: Structure -> String
+getStructureString struct =
+  case struct of
+    Structure str -> str
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) =
+  Structure (a <> b)
+
+render :: Html -> String
+render html =
+  case html of
+    Html str -> str
+
+escape :: String -> String
+escape =
+  let escapeChar c =
+        case c of
+          '<' -> "&lt;"
+          '>' -> "&gt;"
+          '&' -> "&amp;"
+          '"' -> "&quot;"
+          _ -> [c]
+   in concatMap escapeChar
