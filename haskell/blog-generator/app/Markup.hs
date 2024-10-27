4c8560b010983bd4502a58f288ce9026ddf26818
yuk1ty <yuk1ty@users.noreply.github.com>
2024-10-27 22:52:49 +0900

    Parse Markup

--- haskell/blog-generator/app/Markup.hs
+++ haskell/blog-generator/app/Markup.hs
@@ -11,3 +11,21 @@ data Structure
   | UnorderedList [String]
   | OrderedList [String]
   | CodeBlock [String]
+
+parse :: String -> Document
+parse = parseLines [] . lines
+
+parseLines :: [String] -> [String] -> Document
+parseLines currentParagraph txts =
+  let paragraph = Paragraph (unlines (reverse currentParagraph))
+   in case txts of
+        [] -> [paragraph]
+        currentLine : rest ->
+          if trim currentLine == ""
+            then
+              paragraph : parseLines [] rest
+            else
+              parseLines (currentLine : currentParagraph) rest
+
+trim :: String -> String
+trim = unwords . words
