f5c0ce3d1d60656f053c3962f0a51556d32f26c3
yuk1ty <yuk1ty@users.noreply.github.com>
2025-04-06 15:09:52 +0900

    Add rustdoc to add description

--- rust/mcp-server-trial/src/main.rs
+++ rust/mcp-server-trial/src/main.rs
@@ -1,10 +1,12 @@
 use poem_mcpserver::{McpServer, Tools, stdio::stdio, tool::Text};
 use uuid::Uuid;
 
+/// "UUIDv4 Generator"
 struct UuidGenerator;
 
 #[Tools]
 impl UuidGenerator {
+    /// Generate a new UUIDv4
     async fn generate_v4(&self) -> Text<String> {
         Text(Uuid::new_v4().to_string())
     }
