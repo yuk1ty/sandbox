f66e38ff3ed3b432159d841cd31d47fb10e10139
yuk1ty <yuk1ty@users.noreply.github.com>
2025-04-20 21:10:26 +0900

    Add smol HTTP server and update README

--- rust/http-server-from-scratch/README.md
+++ rust/http-server-from-scratch/README.md
@@ -2,7 +2,7 @@
 
 ...in Rust. A trial of making an HTTP server that just echoes incoming requests. This toy example effectively illustrates
 
-- What is the difference between sync and async implementation in Rust.
+- What is the difference between sync and async implementation (both tokio and smol) in Rust.
 - How we can migrate a sync implementation to an async one that leverages on tokio.
 
 ## Launch
@@ -15,6 +15,9 @@ cargo run -p sync-http-server
 
 # Run the async server
 cargo run -p async-http-server
+
+# Run the smol version async server
+cargo run -p async-smol-http-server
 ```
 
 Then you can check it works on the browser with accessing `127.0.0.1:9999`.
---
+++ rust/http-server-from-scratch/async-smol-http-server/src/main.rs
@@ -0,0 +1,33 @@
+use smol::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::{TcpListener, TcpStream},
+    stream::StreamExt,
+};
+
+fn main() {
+    smol::block_on(async {
+        let listener = TcpListener::bind("127.0.0.1:9999").await.unwrap();
+        loop {
+            let (stream, _) = listener.accept().await.unwrap();
+            handle_connection(stream).await;
+        }
+    })
+}
+
+async fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(stream.clone());
+
+    let mut req = vec![];
+    let mut lines = buf_reader.lines();
+    while let Some(line) = lines.next().await {
+        let line = line.unwrap();
+        if line.is_empty() {
+            break;
+        }
+        req.push(line);
+    }
+
+    let res = format!("HTTP/1.1 200 OK\r\n\r\n{:#?}", req);
+    stream.write_all(res.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
+}
