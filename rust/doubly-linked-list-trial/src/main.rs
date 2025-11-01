b48c3d450f82f69fdec8e91f87008059539accf8
yuk1ty <yuk1ty@users.noreply.github.com>
2025-11-01 22:58:47 +0900

    Implement Rc version

--- rust/doubly-linked-list-trial/src/main.rs
+++ rust/doubly-linked-list-trial/src/main.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 pub struct List<T> {
     head: Link<T>,
 }
@@ -7,40 +9,61 @@ impl<T> List<T> {
         List { head: None }
     }
 
-    pub fn push(&mut self, elem: T) {
-        let new_node = Box::new(Node {
-            elem,
-            next: self.head.take(),
-        });
-        self.head = Some(new_node);
+    pub fn prepend(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem,
+                next: self.head.clone(),
+            })),
+        }
     }
 
-    pub fn pop(&mut self) -> Option<T> {
-        self.head.take().map(|node| {
-            self.head = node.next;
-            node.elem
-        })
-    }
-
-    pub fn peek(&self) -> Option<&T> {
+    pub fn head(&self) -> Option<&T> {
         self.head.as_ref().map(|node| &node.elem)
     }
 
-    pub fn peek_mut(&mut self) -> Option<&mut T> {
-        self.head.as_mut().map(|node| &mut node.elem)
+    pub fn tail(&self) -> List<T> {
+        List {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
     }
 }
 
 impl<T> Drop for List<T> {
     fn drop(&mut self) {
-        let mut cur_link = self.head.take();
-        while let Some(mut boxed_node) = cur_link {
-            cur_link = boxed_node.next.take();
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(node) {
+                head = node.next.take()
+            } else {
+                break;
+            }
         }
     }
 }
 
-type Link<T> = Option<Box<Node<T>>>;
+type Link<T> = Option<Rc<Node<T>>>;
 
 struct Node<T> {
     elem: T,
